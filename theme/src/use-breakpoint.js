56670406293c2b945dc4f30a56259f64d9fd95c7
Luke Karrys <luke@lukekarrys.com>
2023-09-24 10:31:58 -0700

    fix: make media query hook work during SSR

--- theme/src/use-breakpoint.js
+++ theme/src/use-breakpoint.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import {ThemeContext} from 'styled-components'
 
+function useMatchMedia(query) {
+  return React.useMemo(() => {
+    if (typeof window === 'undefined') {
+      const ev = new EventTarget()
+      ev.matches = false
+      return ev
+    }
+    return window.matchMedia(query)
+  }, [query])
+}
+
 function useBreakpoint(index, minMax = 'min') {
   const theme = React.useContext(ThemeContext)
-
-  const matchMedia = React.useMemo(() => {
-    const px = parseInt(theme.breakpoints[index])
-    return window.matchMedia(`(${minMax}-width: ${px - (minMax === 'min' ? 0 : 1)}px)`)
-  }, [theme, index, minMax])
+  const px = parseInt(theme.breakpoints[index])
+  const matchMedia = useMatchMedia(`(${minMax}-width: ${px - (minMax === 'min' ? 0 : 1)}px)`)
 
   const [matches, setMatches] = React.useState(matchMedia.matches)
   const handleChange = React.useCallback(() => setMatches(matchMedia.matches), [matchMedia])
